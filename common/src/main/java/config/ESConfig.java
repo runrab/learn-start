a77cceddb60635a726d4ba37b18510761e83c9ea
ismech <1541649163@qq.com>
2022-01-14 11:06:49 +0800

    更新

---
+++ common/src/main/java/config/ESConfig.java
@@ -0,0 +1,26 @@
+package config;
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+//https://blog.csdn.net/weixin_44335140/article/details/121162331
+
+/**
+ * @author o
+ */
+@Configuration
+public class ESConfig {
+    @Bean
+    public RestHighLevelClient getRestHighLevelClient(){
+        RestHighLevelClient client = new RestHighLevelClient(
+                //如果是集群再配置多个
+                RestClient.builder(new HttpHost("node01",9200,"http"),
+                        new HttpHost("node02",9200,"http"),
+                        new HttpHost("node03",9200,"http")
+                )
+        );
+        return client;
+    }
+}
